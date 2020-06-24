ed8f7e84341990cd0c879149212f3c806304a585
kissiy179 <kissiy179@gmail.com>
2020-06-25 00:01:52 +0900

    スキャン対象ファイル数制限解除が間違っていたのを修正

--- maya_scan_all.py
+++ maya_scan_all.py
@@ -22,7 +22,7 @@ def main(root_dir):
             scn_files.append(os.path.join(dir_, file_))
 
     for i, scn_file in enumerate(scn_files):
-        if scan_count <=0 or i >= scan_count: 
+        if scan_count >=0 and i >= scan_count: 
             break
 
         cmds.file(scn_file, open=True)
