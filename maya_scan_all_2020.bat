2f666671388a1a6a09bc4617b222a7c9e4e5b5af
kissiy179 <kissiy179@gmail.com>
2020-06-25 21:23:53 +0900

    実行バッチファイルをMayaバージョンごとのものに変更

---
+++ maya_scan_all_2020.bat
@@ -0,0 +1,14 @@
+@Echo off
+REM カレントディレクトリを現在のディレクトリに変更
+cd /d %~dp0
+
+REM Mayaバージョンを指定してアプリケーションフォルダを取得(外部から指定可)
+Set MAYA_VER=%1
+if /%MAYA_VER%/==// Set MAYA_VER=2020
+FOR /F "TOKENS=1,2,*" %%I IN ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Autodesk\Maya\%MAYA_VER%\Setup\InstallPath" /v "MAYA_INSTALL_LOCATION"') DO IF "%%I"=="MAYA_INSTALL_LOCATION" SET MAYA_APP_PATH=%%K
+
+REM バッチ処理スクリプトを実行
+REM %MAYA_APP_PATH%bin\mayapy %cd%\maya_scan_all.py
+curl https://raw.githubusercontent.com/kissiy179/maya_scan_all/master/maya_scan_all.py | "%MAYA_APP_PATH%bin\mayapy"
+
+pause
