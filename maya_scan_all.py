a35163e76ad3e8a57343f5d32eb9efa87adb313f
kissiy179 <kissiy179@gmail.com>
2020-06-26 13:50:02 +0900

    エラーでバッチが止まった場合にどこで止まったかわかるようにシーンパスを表示するように変更

--- maya_scan_all.py
+++ maya_scan_all.py
@@ -27,6 +27,7 @@ def main(root_dir):
         if scan_count >=0 and i >= scan_count: 
             break
 
+        print('Load Scene: {}'.format(scn_file))
         reportIssue('', smode=1)
         cmds.file(scn_file, open=True)
         issuesFound, issuesFixed = clean_malware('current scene')
