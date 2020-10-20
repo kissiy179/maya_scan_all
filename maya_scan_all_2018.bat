16199f7f31fb5e19ff56715284e121f93621d585
kissiy179 <kissiy179@gmail.com>
2020-10-20 19:52:12 +0900

    バッチファイルから日本語コメントを除外してエンコードによるエラーを回避

--- maya_scan_all_2018.bat
+++ maya_scan_all_2018.bat
@@ -1,16 +1,16 @@
 @Echo off
-REM カレントディレクトリを現在のディレクトリに変更
+REM Change the current directory.
 cd /d %~dp0
 
-REM Mayaバージョンを指定してアプリケーションフォルダを取得(外部から指定可)
+REM Get the application directory by specifying the Maya version (can be specified externally).
 Set MAYA_VER=%1
 if /%MAYA_VER%/==// Set MAYA_VER=2018
 FOR /F "TOKENS=1,2,*" %%I IN ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Autodesk\Maya\%MAYA_VER%\Setup\InstallPath" /v "MAYA_INSTALL_LOCATION"') DO IF "%%I"=="MAYA_INSTALL_LOCATION" SET MAYA_APP_PATH=%%K
 
-REM バッチ処理スクリプトを実行
+REM Run a batch processing script.
 "%MAYA_APP_PATH%bin\mayapy" %cd%\maya_scan_all.py
 
-REM 実行できなかった場合githubからダウンロードして実行
+REM Download from github and run it if it doesn't work.
 if %errorlevel% neq 0 (
     curl https://raw.githubusercontent.com/kissiy179/maya_scan_all/master/maya_scan_all.py | "%MAYA_APP_PATH%bin\mayapy"
 )
